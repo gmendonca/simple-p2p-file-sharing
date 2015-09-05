814aedada3f479952fbf66fe51b264ab05b4a491
Gustavo Mendonca <gustavogmend@gmail.com>
2015-09-05 18:55:09 -0500

    Peer - Server sides done

--- src/server/Peer.java
+++ src/server/Peer.java
@@ -10,5 +10,69 @@ public class Peer {
 	private String address;
 	private int port;
 	//TODO: how to connect to the peer -address string is an example so far
+	
+	public Peer(int peerId, int numFiles, ArrayList<String> fileNames, String directory, String address, int port){
+		this.peerId = peerId;
+		this.numFiles = numFiles;
+		this.fileNames = new ArrayList<String>();
+		this.fileNames.addAll(fileNames);
+		this.directory = directory;
+		this.address = address;
+		this.port = port;
+	}
+	
+	//getters
+	public int getPeerId(){
+		return peerId;
+	}
+	
+	public int getNumFiles(){
+		return numFiles;
+	}
+	
+	public ArrayList<String> getFileNames(){
+		return fileNames;
+	}
+	
+	public String getDirectory(){
+		return directory;
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
+	public int getPort(){
+		return port;
+	}
+	
+	//setters
+	public void setPeerId(int peerId){
+		this.peerId = peerId;
+	}
+	
+	public void setNumFiles(int numFiles){
+		this.numFiles = numFiles;
+	}
+	
+	public void setFileNames(ArrayList<String> fileNames){
+		this.fileNames.addAll(fileNames);
+	}
+	
+	public void addFileName(String fileName){
+		this.fileNames.add(fileName);
+	}
+	
+	public void setDirectory(String directory){
+		this.directory = directory;
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public void setPort(int port){
+		this.port = port;
+	}
 
 }
