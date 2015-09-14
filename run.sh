8b6f9ee0fb1c3c11ac2c22c2588330e08880a1a5
Gustavo Mendonca <gustavogmend@gmail.com>
2015-09-14 14:41:49 -0500

    script fixed with option to choose server address and port

--- run.sh
+++ run.sh
@@ -8,8 +8,10 @@ compile()
 
 run_server()
 {
-    if [ $# -gt 0 ]; then
-        echo "No arguments required"
+    PORT=$1
+
+    if [ $# -eq 1 ]; then
+        java -classpath bin/ server.CentralIndexingServer $PORT
     else
         java -classpath bin/ server.CentralIndexingServer
     fi
@@ -19,9 +21,13 @@ run_client()
 {
     DIRECTORY=$1
     PORT=$2
+    SERVER=$3
+    SERVERPORT=$4
 
     if [ $# -lt 2 ]; then
         echo "It should be run_client directory port"
+    elif [ $# -lt 2 ]; then
+        java -classpath bin/ client.Client $DIRECTORY $PORT $SERVER $SERVERPORT
     else
         java -classpath bin/ client.Client $DIRECTORY $PORT
     fi
@@ -34,12 +40,18 @@ benchmarking()
     FOLDERNAME=$2
     FILENAME=$3
     NUMREQUESTS=$4
+    SERVER=$5
+    SERVERPORT=$6
 
     if [ $# -lt 4 ]; then
         echo "It should be benchmarking numNodes folderName fileName numRequests"
+    elif [ $# -gt 4 ]; then
+        for ((i=0; i<N; i++)); do
+            java -classpath bin/ bench.Benchmarking $FOLDERNAME $(($PORT+$i)) $FILENAME $NUMREQUESTS $SERVER $SERVERPORT &
+        done
     else
         for ((i=0; i<N; i++)); do
-            java -classpath bin/ bench.Benchmarking $FOLDERNAME $(($PORT+$i)) $FILENAME $NUMREQUESTS &
+            java -classpath bin/ bench.Benchmarking $FOLDERNAME $(($PORT+$i)) $FILENAME $NUMREQUESTS $SERVER $SERVERPORT &
         done
     fi
 
@@ -57,10 +69,18 @@ help(){
             echo "        "
             echo "        Run a Central Indexing Server at port 3434"
             echo "        "
+            echo "        or"
+            echo "        "
+            echo "        run_server port"
+            echo "        "
     elif [ $1 = "run_client" ]; then
             echo "        "
             echo "        Run a Peer"
             echo "        run_client directory port"
+            echo "        "
+            echo "        or"
+            echo "        "
+            echo "        run_client directory port serverAddress serverPort"
             echo "        "
     elif [ $1 = "benchmarking" ]; then
             echo "        "
@@ -71,6 +91,10 @@ help(){
             echo "        - folderName: the benchmarking is a peer so needs a folder name to register to the Server"
             echo "        - fileName: the file you want to search in the Server"
             echo "        - numRequests: number of requests per node"
+            echo "        "
+            echo "        or"
+            echo "        "
+            echo "        benchmarking numNodes folderName fileName numRequests serverAddress serverPort"
             echo "        "
     fi
 }
