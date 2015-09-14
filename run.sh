bcaf7824c83436d5b5e8392b32fa2b65596bd98a
Gustavo Mendonca <gustavogmend@gmail.com>
2015-09-14 12:00:55 -0500

    Added help to the run script

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ run_server()
     java server/CentralIndexingServer
 }
 
-run_a_client()
+run_client()
 {
     DIRECTORY=$1
     PORT=$2
@@ -32,6 +32,36 @@ benchmarking()
     for ((i=0; i<N; i++)); do
         java bench/Benchmarking $FOLDERNAME $(($PORT+$i)) $FILENAME $NUMREQUESTS &
     done
+}
+
+help(){
+    if [ $# -eq 0 ]; then
+        echo "        "
+        echo "        run_server - Run the Central Indexing Server"
+        echo "        run_client - Run a Peer"
+        echo "        benchmarking - Run a Benchmarking with multiple requests to the server"
+        echo "        Type 'help command' to know more about each one"
+        echo "        "
+    elif [ $1 = "run_server" ]; then
+            echo "        "
+            echo "        Run a Central Indexing Server at port 3434"
+            echo "        "
+    elif [ $1 = "run_client" ]; then
+            echo "        "
+            echo "        Run a Peer"
+            echo "        run_client directory port"
+            echo "        "
+    elif [ $1 = "benchmarking" ]; then
+            echo "        "
+            echo "        Run a Benchmarking with multiple requests to the server"
+            echo "        benchmarking numNodes folderName fileName numRequests"
+            echo "        "
+            echo "        - numNodes: number of nodes"
+            echo "        - folderName: the benchmarking is a peer so needs a folder name to register to the Server"
+            echo "        - fileName: the file you want to search in the Server"
+            echo "        - numRequests: number of requests per node"
+            echo "        "
+    fi
 }
 
 #java bench/Benchmarking peer1 4001 nano & java bench/Benchmarking peer2 4002 nano & java bench/Benchmarking peer2 4004 nano
